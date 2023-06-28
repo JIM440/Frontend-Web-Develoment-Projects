a6da63dfafef61c34aa9b35fba674c9b56e73b72
JIM440 <118163334+JIM440@users.noreply.github.com>
2023-06-28 02:05:55 +0100

    initial commit

---
+++ e-restaurants/Burger Delivery ll/script.js
@@ -0,0 +1,11 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = function(){
+    navbar.classList.toggle("active");
+    menu.classList.toggle(bx-X);
+}
+
+    window.onscroll = () => {
+navbar.classList.remove("active");
+    };
